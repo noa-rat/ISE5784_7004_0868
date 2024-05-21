96c78ac374cb1988c8ccff27fd6e6892619aa387
noa-rat <noadvory2004@gmail.com>
2024-05-21 18:28:13 +0300

    half stage 3

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -2,8 +2,11 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Util;
 import primitives.Vector;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Sphere extends RadialGeometry{
@@ -31,6 +34,36 @@ public class Sphere extends RadialGeometry{
      */
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return null;
+        Vector u = center.subtract(ray.getHead());
+        double tm = (ray.getDirection()).dotProduct(u);
+        if ((tm * tm) > u.lengthSquared())
+            return null;
+        double d = Math.sqrt(u.lengthSquared() - (tm * tm));
+        if ((d * d) > (radius * radius) || d >= radius)
+            return null;
+        double th = Math.sqrt((radius * radius) - (d * d));
+        double t1 = tm + th;
+        double t2 = tm - th;
+        List<Point> intersections = new LinkedList<Point>();
+        if ((Util.isZero(t1) && Util.isZero(t2)) || (t1 < 0 && t2 < 0)) {
+            return null;
+        }
+        if (Util.isZero(t1) || t1 < 0) {
+            Point p2 = (ray.getHead()).add((ray.getDirection()).scale(t2));
+            intersections.add(p2);
+            return intersections;
+        }
+        if (Util.isZero(t2) || t2 < 0) {
+            Point p1 = (ray.getHead()).add((ray.getDirection()).scale(t1));
+            intersections.add(p1);
+            return intersections;
+        }
+        else {
+            Point p1 = (ray.getHead()).add((ray.getDirection()).scale(t1));
+            Point p2 = (ray.getHead()).add((ray.getDirection()).scale(t2));
+            intersections.add(p1);
+            intersections.add(p2);
+            return intersections;
+        }
     }
 }
--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -1,8 +1,10 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-import primitives.Point;
-import primitives.Vector;
+import primitives.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SphereTest {
 
-    private final Sphere sphere = new Sphere(2, new Point(0,0,1));
-
     /**
      * Test method for func getNormal in primitives.Sphere
      */
     @Test
     void testGetNormal() {
         // ============ Equivalence Partitions Tests ==============
+        Sphere sphere = new Sphere(2, new Point(0,0,1));
         assertEquals(new Vector(0,0,1), sphere.getNormal(new Point(0,0,3)),
                 "Error: The sphere normals don't correct");
 
@@ -32,7 +33,69 @@ class SphereTest {
     @Test
     void findIntersections() {
         // ============ Equivalence Partitions Tests ==============
+        Sphere sphere = new Sphere(2, new Point(0,0,1));
+
+        var result = sphere.findIntersections(new Ray(new Point(3, 3, 0), new Vector(-1,-1,0)));
+        assertEquals(2, result.size(), "Error: there should be two intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(1, 1, 0), new Vector(-1,-1,0)));
+        assertEquals(1, result.size(), "Error: there should be one intersection point");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(3, 3, 0), new Vector(1,1,0))),
+                "Error: there are not intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(3, 3, 0), new Vector(0,-1,0))),
+                "Error: there are not intersection points");
 
         // =============== Boundary Values Tests ==================
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(2, 0, 1), new Vector(1,0.5,0))),
+                "Error: there are not intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(2, 0, 1), new Vector(-1,-0.5,0)));
+        assertEquals(1,
+                result.size(),
+                "Error: there should be one intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(2, -1, 1), new Vector(0,1,0))),
+                "Error: there are not intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(2, 0, 1), new Vector(0,1,0))),
+                "Error: there are not intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(2, 1, 1), new Vector(0,1,0))),
+                "Error: there are not intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(0, 0, 1), new Vector(1,1,0)));
+        assertEquals(1, result.size(), "Error: there should be one intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(0, -2, 1), new Vector(0,-1,0))),
+                "Error: there are not intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(0, 2, 1), new Vector(0,-1,0)));
+        assertEquals(1, result.size(), "Error: there should be one intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(0, -3, 1), new Vector(0,-1,0))),
+                "Error: there are not intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(0, -1, 1), new Vector(0,-1,0)));
+        assertEquals(1, result.size(), "Error: there should be one intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(0, 3, 1), new Vector(0,-1,0)));
+        assertEquals(2, result.size(), "Error: there should be two intersection points");
+
+        assertEquals(null,
+                sphere.findIntersections(new Ray(new Point(2.5, 0, 1), new Vector(0,1,0))),
+                "Error: there are not intersection points");
+
+        result = sphere.findIntersections(new Ray(new Point(1.5, 0, 1), new Vector(0,1,0)));
+        assertEquals(1, result.size(), "Error: there should be one intersection points");
     }
 }
